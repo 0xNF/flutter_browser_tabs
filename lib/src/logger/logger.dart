cc5cacdcbe16a8a5e2edd12d200e62504cf7d08e
0xnf <0xnf@wintech.com>
2023-09-12 16:19:36 +0900

    Changed out ILogger reference

--- lib/src/logger/logger.dart
+++ lib/src/logger/logger.dart
@@ -1,10 +1,10 @@
-import 'package:dart_ilogger/dart_ilogger.dart';
+import 'package:ilogger/ilogger.dart';
 
 /// public Hookpoint for attaching anything implementing an ILogger
 ILogger get logger => _logger;
-ILogger _logger = const BasicLogger(name: "Flutter_Browser_Tabs_logger");
+ILogger _logger = BasicConsoleLogger(name: "Flutter_Browser_Tabs_logger");
 
 /// Sets a Hooked Logger for the library. If `logger` is `null`, then no messages will be logged
 void setLoggerHook(ILogger? logger) {
-  _logger = logger ?? const NullLogger(name: "Flutter_Browser_Tabs_Null_logger");
+  _logger = logger ?? BasicNullLogger(name: "Flutter_Browser_Tabs_Null_logger");
 }
